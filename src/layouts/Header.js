9d379cc6cfd39d8beabd45facbfadfc414b6f635
Andry Ratsimbazafy <andrypatrick8@gmail.com>
2022-10-02 16:28:03 +0300

    Personal exp to projects

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -12,7 +12,7 @@ const menus = [
     name: "my Portfolio",
     id: "work",
   },
-  { icon: "fa fa-tasks", dkMenuName: "Personal Exp", name: "my Personal Exp", id: "blog" },
+  { icon: "fa fa-rectangle-history", dkMenuName: "Projects", name: "my Projects", id: "blog" },
   {
     icon: "fa fa-envelope-open",
     dkMenuName: "Contact",
